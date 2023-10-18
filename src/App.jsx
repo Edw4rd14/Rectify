8ac927fb1c051da715a7d103f0f44e4def0c1846
Edward <edw4rd20005@gmail.com>
2023-10-18 21:35:14 +0800

    Updated website APIs to allow more than 100 tracks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
+import Plot from "react-plotly.js";
 
 const CLIENT_ID = import.meta.env.VITE_REACT_APP_SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = import.meta.env.VITE_REACT_APP_SPOTIFY_CLIENT_SECRET;
@@ -13,7 +14,11 @@ const App = () => {
   const [errMsg, setErrMsg] = React.useState("");
   const [artists,setArtists] = React.useState("");
   const [token,setToken] = React.useState("");
+  const [playlist, setPlaylist] = React.useState([]);
+  const [artistCount, setArtistCount] = React.useState([]);
   const [recommendations,setRecommendations] = React.useState([]);
+  const [refresh, setRefresh] = React.useState(false)
+  const [artistPlot, setArtistPlot] = React.useState([]);
   const handleChange = (e) => {
     setFormData((prevFormData) => {
       return {
@@ -37,6 +42,7 @@ const App = () => {
     axios(recommendationParam)
     .then(response=>{
       setRecommendations(response.data.tracks.map((track)=>({...track})))
+      setRefresh(true)
       setErrMsg("");
     });
   },[artists])
@@ -71,15 +77,59 @@ const App = () => {
     // const {data} = await axios.get("https://api.spotify.com/v1/playlists/" + id,{headers:{"Content-Type":"application/json",'Authorization': "Bearer " + token}}).then(result=>result.json()).then(data=>console.log(data))
     axios(playlistInfoParam)
     .then((response)=>{
+      setPlaylist([{"name":response.data.name, "owner":response.data.owner.display_name, "imgurl":response.data.images[0].url}])
       const getRandomValues = (array, count) => {
         const shuffled = array.sort(() => 0.5 - Math.random());
         return shuffled.slice(0, count);
       };
-      let tracks = response.data.tracks.items;
       let artistIDs = []; 
+      let artistNames = [];
+      let noOfLoops = Math.ceil((response.data.tracks.total-100)/100); // if noloops = 0 bs
+      let tracks = response.data.tracks.items;
       for (let x=0;x<tracks.length;x++) {
         artistIDs.push(tracks[x].track.artists[0])
+        for(let y=0;y<tracks[x].track.artists.length;y++) {
+          artistNames.push(tracks[x].track.artists[y].name)
+        } 
       }
+      for(let z=1;z<noOfLoops;z++) {
+        const playlistInfoParam = {
+          method: "get",
+          url:`https://api.spotify.com/v1/playlists/${playlistID}`,
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            offset: z*100
+          }
+        }
+        axios(playlistInfoParam)
+        .then((response)=>{
+          let tracks = response.data.tracks.items;
+          for (let x=0;x<tracks.length;x++) {
+            artistIDs.push(tracks[x].track.artists[0])
+            for(let y=0;y<tracks[x].track.artists.length;y++) {
+              artistNames.push(tracks[x].track.artists[y].name)
+            } 
+            // refresh runs handleSubmit, save playlist link, show current playlist header
+          }
+        })
+      }
+      const counter = {};
+      artistNames.forEach(ele => {
+        if (counter[ele]) {
+            counter[ele] += 1;
+        } else {
+            counter[ele] = 1;
+        }
+      });
+      const finalArtists = [];
+      const count = [];
+      for (var key in counter) {
+        finalArtists.push(finalArtists)
+        count.push(counter[key])
+      }
+      setArtistCount(counter)
       const uniqueArtist = [...new Set(artistIDs.map((item)=> item.id))]
       const randomArtists = getRandomValues(uniqueArtist,5);
       setArtists(randomArtists.join(","))
@@ -97,14 +147,32 @@ const App = () => {
         <main className='grow'>
           <div className='text-center mt-12'>
             <h1 className='font-extrabold text-2xl uppercase'>Spotify Song Recommendations</h1> 
-            <p>Songs will be recommended to you based on your playlist inserted below. <br/> The recommendations only work on the first <u>100</u> tracks of your playlist. <br/> Press submit again if you want to refresh your recommendations.</p>
+            <p>Songs will be recommended to you based on your playlist inserted below.</p>
           </div>
           <div className='flex justify-center mt-10'>
-            <input type='text' placeholder='Enter playlist URL here' className='w-1/3 h-8 rounded p-2' value={formData.playlistURL} name="playlistURL" onChange={handleChange}/>
-            <input type='submit' className='mx-4 bg-[#343341] text-center text-white px-2 rounded border-2 border-white uppercase font-bold h-8 hover:text-black hover:bg-white' onClick={handleSubmit}/>
+            <input type='text' placeholder='Enter playlist URL here' className='w-1/2 h-8 rounded p-2' value={formData.playlistURL} name="playlistURL" onChange={handleChange}/>
+            <input type='submit' className='mx-4 bg-[#343341] text-center text-white px-2 rounded uppercase font-bold h-8 hover:text-black hover:bg-white' onClick={handleSubmit}/>
           </div>
           <p className='text-lg text-rose-700 text-center mt-2 mb-10'>{errMsg}</p>
-          <hr className='border-[#181c1c] border-2 w-5/6 mx-auto mb-8'/>
+          <hr className='border-[#181c1c] border-2 w-5/6 mx-auto mb-4'/>
+          <div>
+            {
+              playlist.map((info,key)=>{
+                return(
+                  <>
+                  <h2 className='font-bold text-center'>CURRENT PLAYLIST:</h2>
+                    <div className='w-1/5 bg-[#121212] rounded mx-auto text-center'>
+                      <div className='divide-gray-500 divide-y'>
+                        <img src={info.imgurl} className='w-3/4 mt-4 pt-4 mx-auto'/>
+                        <div className='mx-4 mt-2 text-white'>{info.name}</div>
+                      </div>
+                      <div className='text-[#a2a2a2] mb-3 w-5/6 mx-auto pb-4'>{info.owner}</div>
+                    </div>
+                  </>
+                )
+              })
+            }
+          </div>
           <div className='grid lg:grid-cols-5 gap-6 p-4 w-3/4 mb-8 mx-auto text-center grid-cols-2'>
             {
               recommendations && recommendations.map((track,key)=>{
@@ -126,6 +194,26 @@ const App = () => {
               })
             }
           </div>
+          <div>
+            {
+              refresh ? 
+              <div className='mx-auto bg-[#343341] rounded w-fit text-white p-2 hover:text-black hover:bg-white font-bold'>
+                <button onClick={handleSubmit}>REFRESH</button>
+              </div>
+              :
+              <div></div>
+            }
+          </div>
+          {/* <Plot
+            data={[
+              {
+                labels: artistCount[0],
+                values: artistCount[1],
+                type:'pie'
+              },
+            ]}
+            layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+          /> */}
         </main>
         <footer className='h-14 bg-[#181c1c] uppercase font-bold text-white flex justify-center align-center'>
           <a href="https://github.com/edw4rd14" target='_blank' className='my-auto'>© 2023 Edward Tan | All rights reserved.</a>
